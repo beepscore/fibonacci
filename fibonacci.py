fe73e78c5ea7b675c863d79c07455e796260ee61
beepscore <support@beepscore.com>
2015-05-27 17:42:03 -0700

    In fibonacci.py add fibonacci_iterative. In test_fibonacci.py add corresponding tests.

--- fibonacci.py
+++ fibonacci.py
@@ -37,3 +37,26 @@ class Fibonacci:
         self.results[n] = result
         # print(n, result)
         return result
+
+    def fibonacci_iterative(self, n):
+        """ Iterative solution can require less stack space than recursive solution
+        Keyword argument:
+            n is an integer >= 0
+            return Fibonacci number
+        """
+        # set up. Could move this to an initializer
+        self.results[0] = 0
+        self.results[1] = 1
+
+        if self.results.get(n) is not None:
+            return self.results[n]
+
+        # by incrementing index from 2 to n, loop guarantees
+        # self.results[index - 1] and self.results[index - 2]
+        # already contain values
+        # range excludes upper value
+        for index in range(2, n + 1):
+            result = self.results[index - 1] + self.results[index - 2]
+            self.results[index] = result
+
+        return self.results[n]
--- test/test_fibonacci.py
+++ test/test_fibonacci.py
@@ -40,7 +40,46 @@ class TestFibonacci(unittest.TestCase):
         self.assertEqual(expected, actual,
                          "expected fibonacci({}) == {} but got {}"
                          .format(n, expected, actual))
+        n = 50
+        actual = self.fib.fibonacci(n)
+        expected = 12586269025
+        self.assertEqual(expected, actual,
+                         "expected fibonacci({}) == {} but got {}"
+                         .format(n, expected, actual))
+        n = 100
+        actual = self.fib.fibonacci(n)
+        expected = 354224848179261915075
+        self.assertEqual(expected, actual,
+                         "expected fibonacci({}) == {} but got {}"
+                         .format(n, expected, actual))
 
+    def test_fibonacci_iterative(self):
+        for index in range(0, len(self.fibonacci_numbers)):
+            actual = self.fib.fibonacci_iterative(index)
+            expected = self.fibonacci_numbers[index]
+            self.assertEqual(expected, actual,
+                             "expected fibonacci_iterative({}) == {} but got {}"
+                             .format(index, expected, actual))
+
+    def test_fibonacci_iterative_n_large(self):
+        n = 36
+        actual = self.fib.fibonacci_iterative(n)
+        expected = 14930352
+        self.assertEqual(expected, actual,
+                         "expected fibonacci_iterative({}) == {} but got {}"
+                         .format(n, expected, actual))
+        n = 50
+        actual = self.fib.fibonacci_iterative(n)
+        expected = 12586269025
+        self.assertEqual(expected, actual,
+                         "expected fibonacci_iterative({}) == {} but got {}"
+                         .format(n, expected, actual))
+        n = 100
+        actual = self.fib.fibonacci_iterative(n)
+        expected = 354224848179261915075
+        self.assertEqual(expected, actual,
+                         "expected fibonacci_iterative({}) == {} but got {}"
+                         .format(n, expected, actual))
 
 if __name__ == "__main__":
     unittest.main()
