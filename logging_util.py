e1c683b90f724ac7e6b3d3ba429d698017414945
beepscore <support@beepscore.com>
2018-12-14 16:59:58 -0800

    Move comments

--- logging_util.py
+++ logging_util.py
@@ -5,11 +5,22 @@ import sys
 
 def get_logger(name):
     """
+    Log to stream only. Don't add a handler to log to a file.
+    Let program user decide if they want to pipe stream output to a file e.g.
+        python3 fibonacci.py >> ../fib.log
+        python3 -m unittest >> ../test.log
+
+    References
+    https://12factor.net/logs
+    "logging in an application"
+    https://docs.python-guide.org/writing/logging/
+
     https://stackoverflow.com/questions/22807972/python-best-practice-in-terms-of-logging
     https://stackoverflow.com/questions/28330317/print-timestamp-for-logging-in-python
     https://docs.python.org/3/library/logging.html#formatter-objects
     https://docs.python.org/3.6/howto/logging.html#logging-basic-tutorial
     https://docs.python.org/3.6/howto/logging.html#logging-to-a-file
+
     :param name: logger name
     :return: a configured logger
     """
@@ -21,23 +32,12 @@ def get_logger(name):
 
     # add one or more handlers
 
-    # log to terminal stdout
+    # log stream to terminal stdout. program user can choose to pipe stream to a file.
     screen_handler = logging.StreamHandler(stream=sys.stdout)
     screen_handler.setFormatter(formatter)
     logger.addHandler(screen_handler)
 
-    """
-    comment out log to file.
-    instead log to stream only.
-    let program user decide if they want to pipe stream output to a file e.g.
-        python3 fibonacci.py >> ../fib.log
-        python3 -m unittest >> ../test.log
-    references
-    "logging in an application"
-    https://docs.python-guide.org/writing/logging/
-    https://12factor.net/logs
-    """
-    # log to file
+    # Don't log to file. See docstring for rationale.
     # mode 'a' append, not 'w' write
     # handler = logging.FileHandler('./data/output/fib.log', mode='a')
     # handler.setFormatter(formatter)
